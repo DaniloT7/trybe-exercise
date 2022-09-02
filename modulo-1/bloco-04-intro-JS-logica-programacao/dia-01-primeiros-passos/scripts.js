08638298cba9d0d075520f91d35be29cfa8ede5f
Danilo Cardoso <danilowj010@gmail.com>
2022-09-02 15:39:25 -0300

    Adiciona novas alterações

--- modulo-1/bloco-04-intro-JS-logica-programacao/dia-01-primeiros-passos/scripts.js
+++ modulo-1/bloco-04-intro-JS-logica-programacao/dia-01-primeiros-passos/scripts.js
@@ -114,7 +114,7 @@ else{
 const nota = 50;
 if (nota >= 80){
     console.log ('Parabéns, voce foi aprovado!');
-}else if (nota < 80 && nota >= 60){
+}else if (nota < 80 && nota >= 60){dia-01-primeiros-passos
     console.log ('Você está na nossa lista de espera.');
 }else if (nota < 60){
     console.log ('Você foi reprovado');
